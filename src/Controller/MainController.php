37a22fee7ff50033788b9c43eae2f54df2989f96
WebDevCF2m <gitweb@cf2m.be>
2024-10-15 13:33:54 +0200

    menu visible

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,15 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Section;
 
 // 😊😊
 class MainController extends AbstractController
 {
+    # contient notre menu
+    private array $menu;
+    # contient l'EntityManagerInterface
+    private EntityManagerInterface $em;
+
+    // initialise les valeurs des attributes
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->em = $entityManager;
+        $this->menu = $this->em->getRepository(Section::class)->findAll();
+    }
+
     #[Route('/', name: 'homepage')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'menu' => $this->menu,
         ]);
     }
 }
